5e0ae593f99a615cf54ae64457b7f377d6556b30
Alfredo Arriola Jr <73959568+alfredoarriolajr@users.noreply.github.com>
2023-01-13 01:34:12 -0800

    remove button

--- components/paint.js
+++ components/paint.js
@@ -56,7 +56,7 @@ export default function Home() {
                 token_id: id,
                 metadata: {
                     title: 'Cryptonized.net',
-                    description: 'AI Generated NFT',
+                    description: 'An AI Generated NFT',
                     media: lastItem.output || userUploadedImage,
                 },
                 receiver_id: user,
@@ -176,14 +176,14 @@ export default function Home() {
                                         <p className='text-gray-700 text-base mb-4'>
                                             {token.metadata.description}
                                         </p>
-                                        <button
+                                        {/* <button
                                             onClick={() => {
                                                 handleTransfer(token.token_id);
                                             }}
                                             type='button'
                                             className=' inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'>
                                             Transfer NFT
-                                        </button>
+                                        </button> */}
                                     </div>
                                 </div>
                             ))}
