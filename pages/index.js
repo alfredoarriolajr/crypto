fc46fd6a99d2e2998206193a2ab6af7843179af0
Alfredo Arriola Jr <73959568+alfredoarriolajr@users.noreply.github.com>
2023-01-13 01:07:59 -0800

    text edits

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-
 import Head from 'next/head';
 import Home from '../components/paint';
 
 export default function About() {
-
     return (
-        <div className='max-w-[512px] mx-auto p-10 bg-white rounded-lg'>
+        <div className='max-w-[512px] mx-auto p-4 bg-white rounded-lg'>
             <Head>
                 <title>Cryptonized</title>
                 <meta
@@ -13,11 +11,13 @@ export default function About() {
                     content='initial-scale=1.0, width=device-width'
                 />
             </Head>
-            <p className='pb-5 text-lg'>
-                <strong>Cryptonized.net</strong> Create NFTs from your images
-                and texts. Highlight unwanted parts of the image and let the
-                model generate a new inpainted image with your new idea. Create
-                something special and unique. Powered by Near Protocol
+            <p className='pb-5 text-lg text-center'>
+                <strong>Cryptonized.net</strong>
+            </p>
+            <p>
+                Create NFTs from your images and texts. Highlight unwanted parts
+                of the image and let the model generate a new image with your
+                new idea. Mint your NFTs on the Near Protocol blockchain.
             </p>
             <Home />
             <ol className='list-decimal pl-5'>
@@ -31,9 +31,19 @@ export default function About() {
                 </li>
                 <li className='mb-2'>
                     Refine your text prompt (or leave it untouched) and let the
-                    model generate a new inpainted image.
+                    model generate a new image.
                 </li>
             </ol>
+            <div class='max-w-screen-xl px-4 pt-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8'>
+                <p class='mt-16 text-base leading-6 text-center'>
+                    © 2023 Cryptonized.net by{' '}
+                    <a
+                        href='https://www.linkedin.com/in/alfredoarriolajr/'
+                        className='underline'>
+                        Alfredo Arriola Jr.
+                    </a>
+                </p>
+            </div>
         </div>
     );
 }
